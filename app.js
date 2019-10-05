658b495f819dcafa0f5556c5f152d13c5ce7badd
vadimsenchuk <v.o.senchuk@gmail.com>
2019-10-05 11:53:47 +0300

    change messege

--- app.js
+++ app.js
@@ -12,55 +12,40 @@ const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg, match) => {
   const chatId = msg.chat.id;
-  const resp = `✔️ Вас приветсвует магазинчик *COSA NOSTRA*\nCпасибо, что заглянули к нам.\nЗдесь Вы можете ознакомиться с нашим ассортиментом,\nнайти контакты нашего чата и оператора!\nНаш сайт: http://rost-d.com/box/`;
+  const resp = `👋 Вас приветсвует магазинчик *COSA NOSTRA*\nCпасибо, что заглянули к нам ❗\n➖➖➖➖➖➖➖➖➖\n✔️ Здесь Вы сможете найти и проверить АКТУАЛЬНЫЕ контакты магазина (сайт, боты, операторы) ❗\nПоказать контакты: ➡️/contacts\n➖➖➖➖➖➖➖➖➖\n✔️ Ознакомиться с наличием товара и сеткой цен ❗\nПоказать прайс: ➡️/price\n➖➖➖➖➖➖➖➖➖\n✔️ Быть в курсе всех новостей, акций и скидок магазина ❗`;
 
   bot.sendMessage(chatId, resp, {
-    parse_mode: 'Markdown',
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: 'ПРАЙСЛИСТ',
-            callback_data: 'price'
-          }
-        ],
-        [
-          {
-            text: 'КОНТАКТЫ',
-            callback_data: 'contacts'
-          }
-        ]
-      ]
-    }
+    parse_mode: 'Markdown'
   });
 });
 
-bot.on('callback_query', query => {
-  const id = query.message.chat.id;
-  const data = query.data;
+bot.onText(/\/contacts/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = `Наши контакты:\n📞 +38 (000) 111-11-11\n📞 +38 (111) 222-22-22\n➖➖➖➖➖➖➖➖➖\nНаш сайт:\n🌐 http::/rost-d.com/box/`;
+
+  bot.sendMessage(chatId, resp, {
+    parse_mode: 'Markdown'
+  });
+});
+
+bot.onText(/\/price/, (msg, match) => {
+  const chatId = msg.chat.id;
   let msg = '';
-  if (data === 'contacts') {
-    msg = 'Наши контакты: \n +38 (000) 111-11-11\n +38 (111) 222-22-22';
+  needle.get(url, function(err, res) {
+    if (err) throw err;
+
+    const $ = cheerio.load(res.body);
+    const titles = $('.product-outer h2');
+    const prices = $('.product-outer .woocommerce-Price-amount');
+    const links = $('.product-image-wrapper > a');
+
+    for (let i = 0; i < titles.length; i++) {
+      msg += `👉 <a href="${$(links[i]).attr('href')}">${$(
+        titles[i]
+      ).text()} - ${$(prices[0]).text()}</a>\n`;
+    }
     bot.sendMessage(id, msg, {
-      parse_mode: 'Markdown'
+      parse_mode: 'HTML'
     });
-  } else {
-    needle.get(url, function(err, res) {
-      if (err) throw err;
-
-      const $ = cheerio.load(res.body);
-      const titles = $('.product-outer h2');
-      const prices = $('.product-outer .woocommerce-Price-amount');
-      const links = $('.product-image-wrapper > a');
-
-      for (let i = 0; i < titles.length; i++) {
-        msg += `<a href="${$(links[i]).attr('href')}">${$(
-          titles[i]
-        ).text()} - ${$(prices[0]).text()}</a>\n`;
-      }
-      bot.sendMessage(id, msg, {
-        parse_mode: 'HTML'
-      });
-    });
-  }
+  });
 });
